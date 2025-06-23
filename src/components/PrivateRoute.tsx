5d3c3016f6bfef87dc054ad3b4dd4a7ab66dceb2
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-06-23 16:31:54 -0300

    add PrivateRoute component to protect private pages

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+export function PrivateRoute({ children }: PrivateRouteProps) {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/(auth)/login"); 
+    }
+  }, [isAuthenticated, router]);
+
+
+  if (!isAuthenticated) {
+    return null; 
+  }
+
+  return <>{children}</>;
+}
